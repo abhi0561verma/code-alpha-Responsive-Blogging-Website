3a7d7624683c6ec9b0287a93549a5377f61640d4
ABHISHEK <124080024+abhi0561verma@users.noreply.github.com>
2023-07-31 11:55:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const text=document.querySelector(".Imaginations");
+const textLoad=()=>{
+    setTimeout(()=>{
+        text.textContent="Developer";
+    },0);
+    setTimeout(()=>{
+        text.textContent="Freelancer";
+    },4000);
+    setTimeout(()=>{
+        text.textContent="Designer";
+    },8000);
+}
+textLoad();
+setInterval(textLoad,12000);
+
+const button=document.getElementById("mybutton");
+mybutton.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-mode');
+});
+
+(".fa-solid").click(function () {
+    (".fa-solid").toggleClass("fa-solid fa-moon fa-solid fa-sun ");
+});
